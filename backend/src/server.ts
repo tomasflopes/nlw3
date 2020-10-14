ef44a26ecde9b96ca4a7fe9e9d19a0f32c57a96f
Tomas Lopes <tomas050302@gmail.com>
2020-10-14 11:27:16 +0100

    feat(database): created migrations

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,25 @@
 import express from 'express';
 
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
+
 import './database/connection';
 
 const app = express();
 
 app.use(express.json());
 
+app.post('/orphanages', (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+});
 app.listen(process.env.PORT || 3333);
