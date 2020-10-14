0465a0f282888f6f0c2f845d3cb98e04e548bda3
Tomas Lopes <tomas050302@gmail.com>
2020-10-14 22:19:58 +0100

    feat(error): added error handler

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import 'express-async-errors';
+
+import errorHandler from './errors/handler';
 
 import path from 'path';
 
@@ -12,5 +15,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+
+app.use(errorHandler);
 
 app.listen(process.env.PORT || 3333);
