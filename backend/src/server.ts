4ebff5cbaa71e5337b3beda5b30290327259fed6
Tomas Lopes <tomas050302@gmail.com>
2020-10-14 11:47:36 +0100

    refactor: abstracted controllers

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,25 +1,12 @@
 import express from 'express';
 
-import { getRepository } from 'typeorm';
-import Orphanage from './models/Orphanage';
-
 import './database/connection';
+
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-app.post('/orphanages', (request, response) => {
-  const {
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends
-  } = request.body;
-
-  const orphanagesRepository = getRepository(Orphanage);
-});
 app.listen(process.env.PORT || 3333);
