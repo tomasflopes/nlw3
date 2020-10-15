a4a413bf3db15f86fa40e328d6d355985103f1d3
Tomas Lopes <tomas050302@gmail.com>
2020-10-15 19:08:54 +0100

    feat(refactor): created sidebar component

--- web/src/pages/CreateOrphanage/index.tsx
+++ web/src/pages/CreateOrphanage/index.tsx
@@ -1,41 +1,21 @@
 import React from 'react';
 import { Map, Marker, TileLayer } from 'react-leaflet';
-import L from 'leaflet';
-import { useHistory } from 'react-router-dom';
 
-import { FiArrowLeft, FiPlus } from 'react-icons/fi';
+import { FiPlus } from 'react-icons/fi';
 
-import mapMarkerImg from '../../assets/map-marker.svg';
+import SideBar from '../../components/SideBar';
+import mapIcon from '../../utils/mapIcon';
 
 import '../../styles/pages/create-orphanage.css';
 
-const happyMapIcon = L.icon({
-  iconUrl: mapMarkerImg,
-
-  iconSize: [58, 68],
-  iconAnchor: [29, 68],
-  popupAnchor: [0, -60]
-});
-
 export default function CreateOrphanage() {
-  const { goBack } = useHistory();
-
   return (
     <div id="page-create-orphanage">
-      <aside>
-        <img src={mapMarkerImg} alt="Happy" />
-
-        <footer>
-          <button type="button" onClick={goBack}>
-            <FiArrowLeft size={24} color="#FFF" />
-          </button>
-        </footer>
-      </aside>
-
+      <SideBar />
       <main>
         <form className="create-orphanage-form">
           <fieldset>
-            <legend>Dados</legend>
+            <legend>Data</legend>
 
             <Map
               center={[-27.2092052, -49.6401092]}
@@ -48,25 +28,25 @@ export default function CreateOrphanage() {
 
               <Marker
                 interactive={false}
-                icon={happyMapIcon}
+                icon={mapIcon}
                 position={[-27.2092052, -49.6401092]}
               />
             </Map>
 
             <div className="input-block">
-              <label htmlFor="name">Nome</label>
+              <label htmlFor="name">Name</label>
               <input id="name" />
             </div>
 
             <div className="input-block">
               <label htmlFor="about">
-                Sobre <span>Máximo de 300 caracteres</span>
+                Sobre <span>Máximo of 300 chars</span>
               </label>
               <textarea id="name" maxLength={300} />
             </div>
 
             <div className="input-block">
-              <label htmlFor="images">Fotos</label>
+              <label htmlFor="images">Photos</label>
 
               <div className="uploaded-image"></div>
 
@@ -77,37 +57,35 @@ export default function CreateOrphanage() {
           </fieldset>
 
           <fieldset>
-            <legend>Visitação</legend>
+            <legend>Visit</legend>
 
             <div className="input-block">
-              <label htmlFor="instructions">Instruções</label>
+              <label htmlFor="instructions">Instructions</label>
               <textarea id="instructions" />
             </div>
 
             <div className="input-block">
-              <label htmlFor="opening_hours">Nome</label>
+              <label htmlFor="opening_hours">Name</label>
               <input id="opening_hours" />
             </div>
 
             <div className="input-block">
-              <label htmlFor="open_on_weekends">Atende fim de semana</label>
+              <label htmlFor="open_on_weekends">Open on weekends</label>
 
               <div className="button-select">
                 <button type="button" className="active">
-                  Sim
+                  Yes
                 </button>
-                <button type="button">Não</button>
+                <button type="button">No</button>
               </div>
             </div>
           </fieldset>
 
           <button className="confirm-button" type="submit">
-            Confirmar
+            Confirm
           </button>
         </form>
       </main>
     </div>
   );
 }
-
-// return `https://a.tile.openstreetmap.org/${z}/${x}/${y}.png`;
--- web/src/pages/Orphanage/index.tsx
+++ web/src/pages/Orphanage/index.tsx
@@ -1,37 +1,17 @@
 import React from 'react';
 import { FaWhatsapp } from 'react-icons/fa';
-import { FiClock, FiInfo, FiArrowLeft } from 'react-icons/fi';
+import { FiClock, FiInfo } from 'react-icons/fi';
 import { Map, Marker, TileLayer } from 'react-leaflet';
-import { useHistory } from 'react-router-dom';
-import L from 'leaflet';
 
-import mapMarkerImg from '../../assets/map-marker.svg';
+import SideBar from '../../components/SideBar';
+import mapIcon from '../../utils/mapIcon';
 
 import '../../styles/pages/orphanage.css';
 
-const happyMapIcon = L.icon({
-  iconUrl: mapMarkerImg,
-
-  iconSize: [58, 68],
-  iconAnchor: [29, 68],
-  popupAnchor: [0, -60]
-});
-
 export default function Orphanage() {
-  const { goBack } = useHistory();
-
   return (
     <div id="page-orphanage">
-      <aside>
-        <img src={mapMarkerImg} alt="Happy" />
-
-        <footer>
-          <button type="button" onClick={goBack}>
-            <FiArrowLeft size={24} color="#FFF" />
-          </button>
-        </footer>
-      </aside>
-
+      <SideBar />
       <main>
         <div className="orphanage-details">
           <img
@@ -101,19 +81,19 @@ export default function Orphanage() {
                 />
                 <Marker
                   interactive={false}
-                  icon={happyMapIcon}
+                  icon={mapIcon}
                   position={[-27.2092052, -49.6401092]}
                 />
               </Map>
 
               <footer>
-                <a href="">Ver rotas no Google Maps</a>
+                <a href="">Get the routes on Google Maps</a>
               </footer>
             </div>
 
             <hr />
 
-            <h2>Instruções para visita</h2>
+            <h2>Instructions to the visit</h2>
             <p>
               Venha como se sentir mais à vontade e traga muito amor para dar.
             </p>
@@ -121,19 +101,19 @@ export default function Orphanage() {
             <div className="open-details">
               <div className="hour">
                 <FiClock size={32} color="#15B6D6" />
-                Segunda à Sexta <br />
-                8h às 18h
+                Monday to Friday <br />
+                8h to 18h
               </div>
               <div className="open-on-weekends">
                 <FiInfo size={32} color="#39CC83" />
-                Atendemos <br />
-                fim de semana
+                We're open <br />
+                on weekends
               </div>
             </div>
 
             <button type="button" className="contact-button">
               <FaWhatsapp size={20} color="#FFF" />
-              Entrar em contato
+              Get in touch
             </button>
           </div>
         </div>
