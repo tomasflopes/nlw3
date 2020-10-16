5978b90b7252c1dc45ceab063e99d7bed4d2277f
Tomas Lopes <tomas050302@gmail.com>
2020-10-16 10:00:29 +0100

    fix: fixed boolean bug

--- backend/src/controllers/OrphanageController.ts
+++ backend/src/controllers/OrphanageController.ts
@@ -55,7 +55,8 @@ export default {
       about,
       instructions,
       opening_hours,
-      open_on_weekends
+      open_on_weekends: open_on_weekends === 'true',
+      images
     };
 
     const schema = Yup.object().shape({
